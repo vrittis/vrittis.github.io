c0eb89bc3f3cfa72b7594e1f537a90426cd58f34
vrittis <vrittis>
2014-03-27 23:46:47 +0100

    Big changes
    Added animation, changed save/load mechanism, corrected some font errors
    and css problems

--- stackSurveillance.js
+++ stackSurveillance.js
@@ -1,4 +1,4 @@
-var surveillance = angular.module("surveillance", ['ngCookies','angularLocalStorage']);
+var surveillance = angular.module("surveillance", ['ngAnimate','ngCookies','angularLocalStorage']);
 
 surveillance.controller("StackOverflow", function ($scope, $http, $interval, storage) {
 
@@ -10,6 +10,8 @@ surveillance.controller("StackOverflow", function ($scope, $http, $interval, sto
 			]
 	});
 	
+	$scope.items = {};
+	
 	$scope.refreshAll = function () {
 		console.log("Refresh all queries");
 		for (var i = 0; i < $scope.queries.length; i++) {
@@ -18,11 +20,12 @@ surveillance.controller("StackOverflow", function ($scope, $http, $interval, sto
 	};
 
 	$scope.refresh = function (query) {
-		console.log("Refresh " + query.title);
-		query.results = [];
+		console.log("Refresh \"" + query.title + "\"");
+		$scope.items[query.title] = [];
 		$http.get(query.search).
 		success(function (data) {
-			query.results = data.items;
+			console.log("Refresh \"" + query.title + "\" successful");
+			$scope.items[query.title] = data.items;
 		});
 	}
 
@@ -30,7 +33,6 @@ surveillance.controller("StackOverflow", function ($scope, $http, $interval, sto
 		var query = {"title":$scope.newQueryTitle, "search":$scope.newQuerySearch};
 		$scope.queries.push(query);
 		$scope.refresh(query);
-<<<<<<< HEAD
 	};
 
 	$scope.remove = function (query) {
@@ -40,39 +42,22 @@ surveillance.controller("StackOverflow", function ($scope, $http, $interval, sto
 		}
 	};
 
-	$scope.retrieveQueries = function() {
-		return new Blob(JSON.stringify($scope.queries), {type: "application/json"});
+	$scope.prepareRaw = function() {
+		$scope.stringifiedQueries = JSON.stringify($scope.queries);
+	};
+	
+    $scope.loadRaw = function() {
+		try {
+			var loadedQueries = angular.fromJson($scope.stringifiedQueries);
+			$scope.queries = loadedQueries;
+			$scope.refreshAll();
+		} catch (e) {
+			// something bad happened
+			// we should alert the user
+			console.log("Couldn't read the JSON");
+		}
 	};
 	
 	$scope.refreshAll();
 
-});
-
-surveillance.directive('downloadQueries', function ($compile) {
-    return {
-        restrict:'E',
-        scope:{ getUrlData:'&getData'},
-        link:function (scope, elm, attrs) {
-            var url = URL.createObjectURL(scope.getUrlData());
-            elm.append($compile(
-                '<a class="btn" download="backup.json"' +
-                    'href="' + url + '">' +
-                    'Export settings' +
-                    '</a>'
-            )(scope));
-        }
-    };
-=======
-	};
-
-	$scope.remove = function (query) {
-		var index = $scope.queries.indexOf(query);
-		if (index > -1) {
-			$scope.queries.splice(index, 1);
-		}
-	};
-
-	$scope.refreshAll();
-
->>>>>>> f5e65e094624500e7151daaf1695b80768a5dfec
 });
